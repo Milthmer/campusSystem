e7b7d0737852fc25ab42abed632ab082e1ad813d
Milthmer <1851625156@qq.com>
2026-05-28 19:09:20 +0800

    修改db.js

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-import mysql from "mysql2";
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "895124",
-    database: "campus_navigation",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    host: 'gateway01.ap-northeast-1.prod.aws.tidbcloud.com',
+    port: 4000,
+    user: 'cbdH8H2yNWe8k9A.root',   // 注意：你的截图里用户名是 cbdH8H2yNWe8k9A.root，不是 cbDH8...，请确认
+    password: 'j3TAw74KGRd3NASS',
+    database: 'campus_navigation',
+    ssl: { rejectUnauthorized: false }   // 跳过证书验证，但加密连接仍会建立
 });
 
 export default pool.promise();
