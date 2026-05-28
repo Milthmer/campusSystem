7fa3c08775371066ca094dbe2289a0307e8e58ad
Milthmer <1851625156@qq.com>
2026-05-28 19:28:39 +0800

    测试

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const pool = mysql.createPool({
     host: 'gateway01.ap-northeast-1.prod.aws.tidbcloud.com',
     port: 4000,
-    user: 'cbdH8H2yNWe8k9A.root',   // 注意：你的截图里用户名是 cbdH8H2yNWe8k9A.root，不是 cbDH8...，请确认
+    user: 'cbDH8H2yNWe8k9A.root',   // 注意：你的截图里用户名是 cbdH8H2yNWe8k9A.root，不是 cbDH8...，请确认
     password: 'j3TAw74KGRd3NASS',
     database: 'campus_navigation',
     ssl: { rejectUnauthorized: false }   // 跳过证书验证，但加密连接仍会建立
